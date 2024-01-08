08b606e643e4261c4eed83f205289ba1f17a35f4
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-01-07 22:29:40 -0500

    Update 11. Container With Most Water SOLVED.ts

--- 11/11. Container With Most Water SOLVED.ts
+++ 11/11. Container With Most Water SOLVED.ts
@@ -1,1 +1,11 @@
-test
+function maxArea(height: number[]): number {
+    let maxarea = 0, left = 0, right = height.length - 1;
+    while (left < right) {
+        maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * (right - left));
+        if (height[left] < height[right])
+            left++;
+        else
+            right--;
+    }
+    return maxarea;
+}
