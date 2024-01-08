965ddbdca0a8a94e5631912633d42aec278705f9
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-01-08 18:32:43 -0500

    Update 17. Letter Combinations of a Phone Number SOLVED C#.cs

--- 17/17. Letter Combinations of a Phone Number SOLVED C#.cs
+++ 17/17. Letter Combinations of a Phone Number SOLVED C#.cs
@@ -1,1 +1,24 @@
-
+public class Solution {
+    public IList<string> LetterCombinations(string digits) {
+        if (string.IsNullOrEmpty(digits)) return new List<string>();
+        
+        var phone = new Dictionary<char, string> {
+            {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},
+            {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
+        };
+        
+        var result = new List<string> {""};
+        
+        foreach (var digit in digits) {
+            var temp = new List<string>();
+            foreach (var res in result) {
+                foreach (var ch in phone[digit]) {
+                    temp.Add(res + ch);
+                }
+            }
+            result = temp;
+        }
+        
+        return result;
+    }
+}
