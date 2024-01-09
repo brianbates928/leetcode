9eacfc00579d1ef8e86d70c4a2bb4fc8e2fd737c
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-01-09 18:49:52 -0500

    Create 35. Search Insert Position SOLVED.cs

---
+++ 35/35. Search Insert Position SOLVED.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        //binary search
+        int low = 0;
+        int high = nums.Length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+}
