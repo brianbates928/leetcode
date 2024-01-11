526197e00f50d57a954fa7deca5bc98a98cf4fc5
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-01-10 22:23:10 -0500

    Create 3. Longest Substring Without Repeating Characters SOLVED.cs

---
+++ 3/3. Longest Substring Without Repeating Characters SOLVED.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int n = s.Length;
+        //HashSet to store the characters in the current substring
+        HashSet<char> set = new HashSet<char>();
+        
+        // max length of the substring (ans), and two pointers for the current substring (i and j)
+        int ans = 0, i = 0, j = 0;
+        
+        // Start a loop that continues until reach the end of the string
+        while (i < n && j < n) {
+            // If the character at position j is not in the set, add it to the set,
+            // increment j, and update the maximum length of the substring
+            if (!set.Contains(s[j])){
+                set.Add(s[j++]);
+                //ans keeps track of longest found 
+                ans = Math.Max(ans, j - i);
+            }
+            // If the character at position j is in the set, 
+            // clear the set and start with substring using rest of the string
+            else {
+                set.Remove(s[i++]);
+            }
+        }
+        return ans;
+    }
+}
