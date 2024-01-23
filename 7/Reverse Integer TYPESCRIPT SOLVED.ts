b700e2bba936368c055cafc5a2908d3f943b0977
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-01-23 11:31:56 -0500

    Update and rename Reverse Integer TPYESCRIPT SOLVED.ts to Reverse Integer TYPESCRIPT SOLVED.ts

--- 7/Reverse Integer TYPESCRIPT SOLVED.ts
+++ 7/Reverse Integer TYPESCRIPT SOLVED.ts
@@ -1,5 +1,8 @@
 function reverse(x: number): number {
     let rev = 0;
+    if (x > 0x7FFFFFFF || x < -0x80000000) {
+        return 0;
+    }
     while (x !== 0) {
         rev = rev * 10 + x % 10;
         x = (x > 0) ? Math.floor(x / 10) : Math.ceil(x / 10);
