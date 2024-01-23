0296ce7f6c1cc51dc3209505933531c0304bfc0f
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-01-23 11:21:19 -0500

    Create Reverse Integer TPYESCRIPT SOLVED.ts

---
+++ 7/Reverse Integer TPYESCRIPT SOLVED.ts
@@ -0,0 +1,11 @@
+function reverse(x: number): number {
+    let rev = 0;
+    while (x !== 0) {
+        rev = rev * 10 + x % 10;
+        x = (x > 0) ? Math.floor(x / 10) : Math.ceil(x / 10);
+        if (rev > 0x7FFFFFFF || rev < -0x80000000) {
+            return 0;
+        }
+    }
+    return rev;
+}
