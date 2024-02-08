f225bdf94b57758fb83e6276623ccc94324cbb31
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-02-07 22:40:44 -0500

    Create 279. Perfect Squares TYPESCRIPT SOLVED.ts

---
+++ 279/279. Perfect Squares TYPESCRIPT SOLVED.ts
@@ -0,0 +1,10 @@
+function numSquares(n: number): number {
+    let dp: number[] = Array(n + 1).fill(n);
+    dp[0] = 0;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j * j <= i; j++) {
+            dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+    return dp[n];
+};
