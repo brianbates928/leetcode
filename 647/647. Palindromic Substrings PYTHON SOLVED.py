a06bd51deee723b2f6e9e594e28949d5908341bb
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-02-09 21:53:37 -0500

    Update 647. Palindromic Substrings PYTHON SOLVED.py

--- 647/647. Palindromic Substrings PYTHON SOLVED.py
+++ 647/647. Palindromic Substrings PYTHON SOLVED.py
@@ -8,11 +8,11 @@ class Solution(object):
         dp = [[False]*n for _ in range(n)]
         count = 0
 
-        for len in range(1, n+1):
-            for i in range(n - len + 1):
-                j = i + len - 1
+        for length in range(1, n+1):  # Changed 'len' to 'length'
+            for i in range(n - length + 1):
+                j = i + length - 1
                 if s[i] == s[j]:
-                    dp[i][j] = len < 3 or dp[i+1][j-1]
+                    dp[i][j] = length < 3 or dp[i+1][j-1]
                 if dp[i][j]:
                     count += 1
         return count
